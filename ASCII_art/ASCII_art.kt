612c6333642ea2ee418cf0a371d3e220fb8ce01c
Enes <101250430+EXEcode123@users.noreply.github.com>
2023-12-27 00:09:18 +0100

    Add ASCII_art directory

---
+++ ASCII_art/ASCII_art.kt
@@ -0,0 +1,28 @@
+// Robot draw with ASCII art
+// Language: Kotlin
+
+/* A multiline comment to draft the image
+     .....
+  .         .
+ .   0  0    .
+  ...........
+   .. __ ..
+  . .|C_|. .
+ .  ......  .
+     .  .
+     .  .
+     .  .
+*/
+
+fun main() {
+    println("     .....")
+    println("  .         .")
+    println(" .   0  0    .")
+    println("  ...........")
+    println("   .. __ ..")
+    println("  . .|C_|. .")
+    println(" .  ......  .")
+    println("     .  .")
+    println("     .  .")
+    println("     .  .")
+}
